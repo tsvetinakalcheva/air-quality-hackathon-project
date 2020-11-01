e548c404503ed76d5205339468ef7b7e4e3af960
desis <daggerista@abv.bg>
2020-11-01 11:02:48 +0200

    Generate project from Spring Initializer #1

---
+++ src/main/java/bg/startit/hackathon/airquiality/AirquialityApplication.java
@@ -0,0 +1,13 @@
+package bg.startit.hackathon.airquiality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirquialityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirquialityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/startit/hackathon/airquiality/AirquialityApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.startit.hackathon.airquiality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirquialityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
