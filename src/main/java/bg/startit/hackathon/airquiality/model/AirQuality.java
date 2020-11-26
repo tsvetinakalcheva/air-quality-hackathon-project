799da731a7ebd6e1020861f44089ed62f6df75ba
Georgi Boichev <gboichev56@abv.bg>
2020-11-26 17:14:36 +0200

    Added AirQualityAlerterService Added method in AirQualityRepository Reduced number of pollutant in Enum - AirQuality model
    !!!WE HAVE A PROBLEM HERE!!!

--- src/main/java/bg/startit/hackathon/airquiality/model/AirQuality.java
+++ src/main/java/bg/startit/hackathon/airquiality/model/AirQuality.java
@@ -2,6 +2,8 @@ package bg.startit.hackathon.airquiality.model;
 
 
 import java.time.OffsetDateTime;
+import java.util.HashMap;
+import java.util.Map;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -18,39 +20,63 @@ import java.util.Date;
 @AllArgsConstructor
 @EqualsAndHashCode
 public class AirQuality {
-    @Id
-    //Auto-generate ID
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "airGenerator")
-    @SequenceGenerator(name = "airGenerator", initialValue = 100)
 
-    private Long id;
+  @Id
+  //Auto-generate ID
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "airGenerator")
+  @SequenceGenerator(name = "airGenerator", initialValue = 100)
 
-    // where
-    @NotBlank
-    @Size(min = 2,max = 2)
-    private String country; // network_countrycode
-    @NotBlank
-    private String stationName; // station_name
-    @NotBlank
-    private String stationCode; // station_code
+  private Long id;
 
-    // what
-    @NotNull
-    @Enumerated(EnumType.STRING)
-    private Pollutant pollutant; // pollutant
-    @NotBlank
-    private String unit; // value_unit
-    private double value; // value_numeric
+  // where
+  @NotBlank
+  @Size(min = 2, max = 2)
+  private String country; // network_countrycode
+  @NotBlank
+  private String stationName; // station_name
+  @NotBlank
+  private String stationCode; // station_code
 
-    // when
-    @NotNull
-    private OffsetDateTime timestamp; // value_datetime_inserted = 2020-11-02 07:34:10+01:00
+  // what
+  @NotNull
+  @Enumerated(EnumType.STRING)
+  private Pollutant pollutant; // pollutant
+  @NotBlank
+  private String unit; // value_unit
+  private double value; // value_numeric
 
-    public enum Pollutant {
-        CO, C6H5_CH3, PM10,
-        H2S, NO, PM2_5, SO2, C6H6,
-        NOX, NM3, NO2, C6H4_CH3, NH3,
-        NOX_as_NO2, O3
+  // when
+  @NotNull
+  private OffsetDateTime timestamp; // value_datetime_inserted = 2020-11-02 07:34:10+01:00
+
+  public void getValueAndPollutant(double value_numeric,
+      Pollutant pollutant) {
+    Pollutant co = pollutant.CO;
+    Pollutant o3 = pollutant.O3;
+    Pollutant pm2_5 = pollutant.PM2_5;
+    Pollutant pm10 = pollutant.PM10;
+    Pollutant no2 = pollutant.NO2;
+    Pollutant so2 = pollutant.SO2;
+    Pollutant nh3 = pollutant.NH3;
+//
+    Map<Pollutant, Double> valueAndPollutant = new HashMap<>();
+
+    valueAndPollutant.put(co, 0.);
+    valueAndPollutant.put(o3, 0.);
+    valueAndPollutant.put(pm2_5, 0.);
+    valueAndPollutant.put(pm10, 0.);
+    valueAndPollutant.put(no2, 0.);
+    valueAndPollutant.put(so2, 0.);
+    valueAndPollutant.put(nh3, 0.);
+
+    if (valueAndPollutant.containsKey(pollutant.CO.name())){
+
     }
+  }
+
+  public enum Pollutant {
+    O3, PM2_5, PM10,
+    NO2, SO2, CO, NH3
+  }
 
 }
--- src/main/java/bg/startit/hackathon/airquiality/repository/AirQualityRepository.java
+++ src/main/java/bg/startit/hackathon/airquiality/repository/AirQualityRepository.java
@@ -1,8 +1,11 @@
 package bg.startit.hackathon.airquiality.repository;
 
 import bg.startit.hackathon.airquiality.model.AirQuality;
+import bg.startit.hackathon.airquiality.model.AirQuality.Pollutant;
 import java.time.OffsetDateTime;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -18,4 +21,5 @@ public interface AirQualityRepository extends JpaRepository<AirQuality, Long> {
 
   void deleteByTimestampBefore(OffsetDateTime timestamp);
 
+  List<AirQuality> findByTimestampAfterOrderByTimestamp(OffsetDateTime when);
 }
